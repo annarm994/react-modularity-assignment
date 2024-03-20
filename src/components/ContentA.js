6f53b9ec58d945bfbe2a5dd7994951191a19d5c9
annarm994 <155604262+annarm994@users.noreply.github.com>
2024-03-20 10:16:21 +0500

    Add files via upload

---
+++ src/components/ContentA.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const ContentA = () => {
+  return (
+    <div>
+      <h2>Content A Component</h2>
+      <p>This is Content A.</p>
+    </div>
+  );
+};
---
+++ src/components/ContentB.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const ContentB = () => {
+  return (
+    <div>
+      <h2>Content B Component</h2>
+      <p>This is Content B.</p>
+    </div>
+  );
+};
